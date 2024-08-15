dd0426fd2765b09fd00915350bc23fdbc8da026d
Sinan Talha KOSAR <sinantalhakosar@gmail.com>
2024-08-15 15:03:42 +0300

    feat: Read github repository configs in collator
    Signed-off-by: Sinan Talha KOSAR <sinantalhakosar@gmail.com>

--- plugins/search-backend-module-github-md-docs/src/collators/GithubMDCollatorFactory.ts
+++ plugins/search-backend-module-github-md-docs/src/collators/GithubMDCollatorFactory.ts
@@ -5,23 +5,38 @@ import {
   DocumentCollatorFactory,
   IndexableDocument,
 } from '@backstage/plugin-search-common';
+import { Repository } from '../types';
 
 export interface GithubMDDocument extends IndexableDocument {}
 
 export type GithubMDCollatorFactoryOptions = {
   logger: LoggerService;
+  repositoryList?: Array<Repository>;
 };
 
 export class GithubMDCollatorFactory implements DocumentCollatorFactory {
   private readonly logger: LoggerService;
   public readonly type: string = 'github-md-docs';
 
-  private constructor(options: GithubMDCollatorFactoryOptions) {
+  private readonly repositoryList: Array<Repository> | undefined;
+
+  private constructor(
+    options: GithubMDCollatorFactoryOptions,
+    repositoryList?: Array<Repository>,
+  ) {
     this.logger = options.logger;
+    this.repositoryList = repositoryList;
   }
 
-  static fromConfig(_config: Config, options: GithubMDCollatorFactoryOptions) {
-    return new GithubMDCollatorFactory(options);
+  static fromConfig(config: Config, options: GithubMDCollatorFactoryOptions) {
+    const repoList: Array<Repository> = config
+      .getConfigArray('backend.search.github.sources')
+      .map(source => ({
+        owner: source.get<Repository['owner']>('owner'),
+        repo: source.get<Repository['repo']>('repo'),
+      }));
+
+    return new GithubMDCollatorFactory(options, repoList);
   }
 
   async getCollator() {
@@ -29,6 +44,19 @@ export class GithubMDCollatorFactory implements DocumentCollatorFactory {
   }
 
   async *execute(): AsyncGenerator<GithubMDDocument> {
-    this.logger.info(`Executed GithubMDCollatorFactory`);
+    if (!this.repositoryList) {
+      this.logger.error(
+        'No backend.search.github.sources configured in your app-config.yaml',
+      );
+      return;
+    }
+
+    for (const repository of this.repositoryList) {
+      const owner = repository.owner;
+      const name = repository.repo;
+      const apiUrl = `https://api.github.com/repos/${owner}/${name}/git/trees/main?recursive=1`;
+
+      this.logger.info(`GithubMDCollatorFactory repo ${apiUrl}`);
+    }
   }
 }
